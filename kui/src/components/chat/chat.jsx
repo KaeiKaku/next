6d039e8e9c37b124c30a1572966bccc514ca660e
Kaei Kaku <gerrin.kaku@jp.ey.com>
2025-10-07 12:46:45 +0900

    chatbot_ui_1007

--- kui/src/components/chat/chat.jsx
+++ kui/src/components/chat/chat.jsx
@@ -1,12 +1,31 @@
-import { Flex } from "antd";
+import { Flex, Typography } from "antd";
 import styles from "./chat.module.css";
 import QueryBox from "../queryBox/queryBox";
+
+const { Paragraph } = Typography;
 
 export default function Chat() {
   return (
     <>
       <Flex justify="center" className={styles.chat_con}>
-        <h3>chat</h3>
+        <Flex className={styles.thread_con} vertical>
+          <Typography className={styles.user_thread_con}>
+            <Paragraph>
+              <pre>user block ... </pre>
+            </Paragraph>
+          </Typography>
+          <Typography>
+            <Paragraph>
+              AI response...AI response...AI response...AI response... AI
+              response...AI response...AI response...AI response...AI
+              response...AI response...AI response... AI response...AI
+              response...AI response...AI response... AI response...AI
+              response...AI response...AI response...AI response...AI
+              response...AI response...AI response... AI response...AI
+              response...AI response...
+            </Paragraph>
+          </Typography>
+        </Flex>
         <QueryBox />
       </Flex>
     </>
--- kui/src/components/queryBox/queryBox.jsx
+++ kui/src/components/queryBox/queryBox.jsx
@@ -1,11 +1,27 @@
-import { Flex } from "antd";
+import { useState } from "react";
+import { Flex, Input, Button } from "antd";
+import { SendOutlined } from "@ant-design/icons";
 import styles from "./queryBox.module.css";
 
+const { TextArea } = Input;
+
 export default function QueryBox() {
+  const [value, setValue] = useState("");
   return (
     <>
       <Flex justify="center" className={styles.queryBox_con}>
-        <h3>query</h3>
+        <Flex className={styles.textarea_con} vertical>
+          <TextArea
+            value={value}
+            variant="borderless"
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="send a message..."
+            autoSize={{ minRows: 1, maxRows: 10 }}
+          />
+          <Flex justify="flex-end">
+            <Button shape="circle" icon={<SendOutlined rotate={270} />} />
+          </Flex>
+        </Flex>
       </Flex>
     </>
   );
