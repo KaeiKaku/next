c6965f0c2d571d6a6bab725f84fbdc3ce6657fe4
Kaei Kaku <gerrin.kaku@jp.ey.com>
2025-11-05 14:15:25 +0900

    ui_1105

--- kui/src/App.jsx
+++ kui/src/App.jsx
@@ -2,15 +2,15 @@ import "./App.css";
 import "@ant-design/v5-patch-for-react-19";
 import { App } from "antd";
 import Home from "@/components/home/home";
-import SessionModal from "@/components/sessionModal/sessionModal";
-import useSessionModal from "@/hook/useSessionModal";
+// import SessionModal from "@/components/sessionModal/sessionModal";
+// import useSessionModal from "@/hook/useSessionModal";
 
 function UiApp() {
-  const [showIntro, confirmIntro] = useSessionModal("hasSeenIntro");
+  // const [showIntro, confirmIntro] = useSessionModal("hasSeenIntro");
 
   return (
     <>
-      <SessionModal visible={showIntro} onConfirm={confirmIntro}></SessionModal>
+      {/* <SessionModal visible={showIntro} onConfirm={confirmIntro}></SessionModal> */}
       <App>
         <Home />
       </App>
--- kui/src/components/chat/chat.jsx
+++ kui/src/components/chat/chat.jsx
@@ -11,6 +11,8 @@ import {
 import { SendOutlined } from "@ant-design/icons";
 import { statusService } from "@/status/status";
 import { apiService } from "@/service/api.service";
+import useSessionModal from "@/hook/useSessionModal";
+import SessionModal from "@/components/sessionModal/sessionModal";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import styles from "./chat.module.css";
@@ -19,6 +21,7 @@ const { Paragraph } = Typography;
 const { TextArea } = Input;
 
 export default function Chat() {
+  const [showIntro, confirmIntro] = useSessionModal("hasSeenIntro");
   const [fetchingAIResponse, setFetchingAIResponse] = useState(false);
   const [query, setQuery] = useState();
   const [documentCollection, setDocumentCollection] = useState();
@@ -169,6 +172,20 @@ export default function Chat() {
   }, []);
 
   useEffect(() => {
+    if (showIntro) {
+      const new_messages = [
+        {
+          query: "",
+          response: "",
+          similarityInfo: "",
+          isShowIntro: true,
+        },
+      ];
+      setMessages((prev) => [...prev, ...new_messages]);
+    }
+  }, [showIntro]);
+
+  useEffect(() => {
     if (latestMessageRef.current) {
       latestMessageRef.current.scrollIntoView({
         behavior: "smooth",
@@ -179,6 +196,7 @@ export default function Chat() {
 
   return (
     <>
+      <SessionModal visible={showIntro} onConfirm={confirmIntro}></SessionModal>
       <Flex
         justify="flest-start"
         align="center"
@@ -248,6 +266,66 @@ export default function Chat() {
                         }
                       )}
                     </Skeleton>
+                  </Typography>
+                )}
+                {message.isShowIntro && (
+                  <Typography
+                    style={{
+                      paddingBottom: last ? "10rem" : 0,
+                    }}
+                  >
+                    {" "}
+                    <Flex
+                      style={{
+                        padding: "1rem",
+                      }}
+                    >
+                      <ul>
+                        <li>
+                          生成AIはプロンプトの入れ方により、表示される回答の精度が異なります。単語のみではなく、文章を入力してください。
+                          <p>例：×「EY新日本」</p>
+                          <p>〇「EY新日本を紹介してください。」</p>
+                        </li>
+                        <li>
+                          当サービスは、以下の関連のドキュメントに対して、
+                          <b>Azure OpenAI</b>
+                          を利用した自動応答を提供するサービス（開発版）です。あらかじめ用意されたドキュメントに対して質問を投げかけ、回答を得ることができます。複数のドキュメントを横断的に検索し、情報を整理することも可能です。
+                        </li>
+                        <li>
+                          <b
+                            style={{
+                              color: "red",
+                            }}
+                          >
+                            クライアントの機密情報や個人情報などの入力は禁止します。
+                          </b>
+                        </li>
+                        <li>
+                          回答には正確性に欠ける場合があるため、回答を疑問視し、内容を慎重に検討した上で、自己判断に基づいてご利用ください。
+                        </li>
+                        <li>
+                          入力された内容は機能向上のためにログを取得します。
+                        </li>
+                        <li style={{ marginTop: "1rem" }}>
+                          <b>ご利用流れ</b>
+                        </li>
+                        <ul
+                          style={{
+                            paddingLeft: "1rem",
+                          }}
+                        >
+                          <li>左のDocument Collectionを選択</li>
+                          <li>
+                            Selection
+                            Queryを検索すると対象Collectionのファイル類似度を計算されます
+                          </li>
+                          <li>
+                            Similarityやフォルダツリーに適当なファイルを選択
+                          </li>
+                          <li>下の会話枠に質問を投げてAIが答えてくれます</li>
+                        </ul>
+                      </ul>
+                    </Flex>
                   </Typography>
                 )}
               </Flex>
